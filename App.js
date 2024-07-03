717156ee12ece5f2d7bc310d7f38a523985153e6
Rakshan Shetty <104481440+Rakshan001@users.noreply.github.com>
2024-07-03 12:20:57 +0530

    Add files via upload
    commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Website from './components/Website';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+    <Website/>
+    <Navbar/>
+    </>
+    
+  );
+}
+
+export default App;
